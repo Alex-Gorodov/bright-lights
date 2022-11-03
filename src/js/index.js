1661fe5c1e591433cfed12cc1ea72778ecf65ec2
Alex Gorodov <a.gorodov.22@gmail.com>
2022-11-03 16:23:28 +0200

    audio ready

--- src/js/index.js
+++ src/js/index.js
@@ -16,37 +16,44 @@ let isPlaying = false;
 
 releazed.onloadedmetadata = function() {
   curTimeLine.max = releazed.duration;
-  };
+};
 
 releazed.ontimeupdate = function() {
   var sec_num = releazed.currentTime;
   var hours = Math.floor(sec_num / 3600);
   var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-  var seconds = sec_num - (hours * 3600) - (minutes * 60);
-  if (seconds == 58) {
+  var seconds = Math.round(sec_num - (hours * 3600) - (minutes * 60));
+  if (seconds != 0 && seconds % 60 == 0) {
     seconds = 0;
     minutes++;
   }
-  seconds = Math.round(seconds);
 
   if (hours < 10) {
-    hours = "0"+hours;
+    hours = "0" + hours;
   }
   if (minutes < 10) {
-    minutes = "0"+minutes;
+    minutes = "0" + minutes;
   }
   if (seconds < 10) {
-    seconds = "0"+seconds;
-  } playTime.innerHTML = minutes+':'+seconds;
+    seconds = "0" + seconds;
+  }
+  playTime.innerHTML = minutes + ':' + seconds;
   if (isPlaying) {
-    curTimeLine.value=releazed.currentTime;
+    curTimeLine.value = releazed.currentTime;
+  }
+  if (sec_num >= curTimeLine.max) {
+    releazed.pause();
+    playBtn.classList.remove('hero__btn-play--pause');
+    playTime.innerHTML = '00:00';
+    curTimeLine.value = 0;
   }
 };
 
-curTimeLine.onchange=function() {
+curTimeLine.onchange = function() {
   releazed.pause();
   releazed.currentTime = curTimeLine.value;
   releazed.play();
+
 };
 
 playBtn.addEventListener("click", (a) => {
@@ -54,9 +61,11 @@ playBtn.addEventListener("click", (a) => {
     releazed.pause();
     isPlaying = false;
     playBtn.ariaLabel = 'play';
+    playBtn.classList.remove('hero__btn-play--pause');
   } else {
     releazed.play();
     isPlaying = true;
+    playBtn.classList.add('hero__btn-play--pause');
     playBtn.ariaLabel = 'pause';
   }
 });
