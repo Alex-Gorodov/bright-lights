869efba63a83aa069aeedcb000e9030aace699b2
Alex Gorodov <a.gorodov.22@gmail.com>
2022-11-10 12:58:19 +0200

    create an array from songs and change methods of Player class for correctly work of player

--- src/js/player.js
+++ src/js/player.js
@@ -1,7 +1,7 @@
 class Player {
   constructor(parentSelector, audio, playBtn, timeLine, curTime, fullTime) {
     this.parent = parentSelector;
-    this.audio = audio;
+    this.audio = Array.from(audio);
     this.playBtn = playBtn;
     this.timeLine = timeLine;
     this.curTime = curTime;
@@ -14,70 +14,80 @@ class Player {
   }
 
   setTimeLineMax() {
-    this.audio.addEventListener('loadedmetadata', () => {
-      this.timeLine.max = Math.floor(this.audio.duration);
-    });
+    for (const song of this.audio) {
+      song.addEventListener('loadedmetadata', () => {
+        this.timeLine.max = Math.floor(song.duration);
+      });
+    }
   }
 
   playMusic() {
     this.playBtn.addEventListener('click', () => {
       this.playBtn.classList.toggle('player__btn-play--pause');
-      if(this.isPlaying) {
-        this.audio.pause();
-        this.isPlaying = false;
-        this.playBtn.ariaLabel = 'play';
-      } else {
-        this.audio.play();
-        this.isPlaying = true;
-        this.playBtn.ariaLabel = 'pause';
+      for (const song of this.audio) {
+        if(this.isPlaying) {
+          song.pause();
+          this.isPlaying = false;
+          this.playBtn.ariaLabel = 'play';
+        } else {
+          song.play();
+          this.isPlaying = true;
+          this.playBtn.ariaLabel = 'pause';
+        }
       }
     });
   }
 
   userTimeMoving() {
     this.timeLine.addEventListener('change', () => {
-      this.audio.currentTime = this.timeLine.value;
+      for (const song of this.audio) {
+        song.currentTime = this.timeLine.value;
+      }
     });
   }
 
   timeUpdate() {
-    this.audio.addEventListener('timeupdate',() => {
-      var sec_num = this.audio.currentTime;
-      var hours = Math.floor(sec_num / 3600);
-      var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-      var seconds = Math.round(sec_num - (hours * 3600) - (minutes * 60));
-      if (seconds != 0 && seconds % 60 == 0) {
-        seconds = 0;
-        minutes++;
-      }
-      if (hours < 10) {
-        hours = '0' + hours;
-      }
-      if (minutes < 10) {
-        minutes = '0' + minutes;
-      }
-      if (seconds < 10) {
-        seconds = '0' + seconds;
-      }
-      this.curTime.innerText = minutes + ':' + seconds;
-      if (this.isPlaying) {
-        this.timeLine.value = this.audio.currentTime;
-      }
-      if (sec_num >= this.timeLine.max) {
-        this.audio.pause();
-        this.playBtn.classList.remove('player__btn-play--pause');
-        this.curTime.innerHTML = '00:00';
-        this.timeLine.value = 0;
-      }
-    });
+    for (const song of this.audio) {
+      song.addEventListener('timeupdate',() => {
+        var sec_num = song.currentTime;
+        var hours = Math.floor(sec_num / 3600);
+        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        var seconds = Math.round(sec_num - (hours * 3600) - (minutes * 60));
+        if (seconds != 0 && seconds % 60 == 0) {
+          seconds = 0;
+          minutes++;
+        }
+        if (hours < 10) {
+          hours = '0' + hours;
+        }
+        if (minutes < 10) {
+          minutes = '0' + minutes;
+        }
+        if (seconds < 10) {
+          seconds = '0' + seconds;
+        }
+        this.curTime.innerText = minutes + ':' + seconds;
+        if (this.isPlaying) {
+          this.timeLine.value = song.currentTime;
+        }
+        if (sec_num >= this.timeLine.max) {
+          song.pause();
+          this.playBtn.classList.remove('player__btn-play--pause');
+          this.curTime.innerHTML = '00:00';
+          this.timeLine.value = 0;
+        }
+      });
+    }
   }
 }
 
 const heroPlayer = new Player(
   document.querySelector('.hero__audio-container'),
-  document.querySelector('.hero__audio'),
+  document.querySelectorAll('.hero__audio'),
   document.querySelector('.hero__btn-play'),
   document.querySelector('.hero__cur-time-line'),
   document.querySelector('.hero__play-time'),
   document.querySelector('.hero__full-time')
 );
+
+console.log(heroPlayer.audio.length);
