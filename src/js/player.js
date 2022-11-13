e5a63cbf50c6c5378cd40554ca0c032bfd323e87
Alex Gorodov <a.gorodov.22@gmail.com>
2022-11-13 06:25:13 +0200

    TODO fix timings

--- src/js/player.js
+++ src/js/player.js
@@ -1,7 +1,8 @@
 class Player {
-  constructor(parentSelector, audio, playBtn, timeLine, curTime, fullTime) {
+  constructor(parentSelector, audio, audioSource, playBtn, timeLine, curTime, fullTime) {
     this.parent = parentSelector;
-    this.audio = Array.from(audio);
+    this.audio = audio;
+    this.audioSource = audioSource;
     this.playBtn = playBtn;
     this.timeLine = timeLine;
     this.curTime = curTime;
@@ -11,83 +12,104 @@ class Player {
     this.playMusic();
     this.timeUpdate();
     this.userTimeMoving();
+    this.setSource();
+    this.songsCounter = 1;
+  }
+
+  setSource() {
+    this.audio.src = this.audioSource[0];
   }
 
   setTimeLineMax() {
-    for (const song of this.audio) {
-      song.addEventListener('loadedmetadata', () => {
-        this.timeLine.max = Math.floor(song.duration);
-      });
-    }
+    this.audio.addEventListener('loadedmetadata', () => {
+      this.timeLine.max = Math.floor(this.audio.duration);
+    });
   }
 
   playMusic() {
     this.playBtn.addEventListener('click', () => {
       this.playBtn.classList.toggle('player__btn-play--pause');
-      for (const song of this.audio) {
-        if(this.isPlaying) {
-          song.pause();
-          this.isPlaying = false;
-          this.playBtn.ariaLabel = 'play';
-        } else {
-          song.play();
-          this.isPlaying = true;
-          this.playBtn.ariaLabel = 'pause';
-        }
+      if(this.isPlaying) {
+        this.audio.pause();
+        this.isPlaying = false;
+        this.playBtn.ariaLabel = 'play';
+      } else {
+        this.audio.play();
+        this.isPlaying = true;
+        this.playBtn.ariaLabel = 'pause';
       }
     });
   }
 
   userTimeMoving() {
     this.timeLine.addEventListener('change', () => {
-      for (const song of this.audio) {
-        song.currentTime = this.timeLine.value;
-      }
+      this.audio.currentTime = this.timeLine.value;
     });
   }
 
   timeUpdate() {
-    for (const song of this.audio) {
-      song.addEventListener('timeupdate',() => {
-        var sec_num = song.currentTime;
-        var hours = Math.floor(sec_num / 3600);
-        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-        var seconds = Math.round(sec_num - (hours * 3600) - (minutes * 60));
-        if (seconds != 0 && seconds % 60 == 0) {
-          seconds = 0;
-          minutes++;
+    this.audio.addEventListener('timeupdate',() => {
+      var sec_num = this.audio.currentTime;
+      var hours = Math.floor(sec_num / 3600);
+      var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+      var seconds = Math.round(sec_num - (hours * 3600) - (minutes * 60));
+      if (seconds != 0 && seconds % 60 == 0) {
+        seconds = 0;
+        minutes++;
+      }
+      if (hours < 10) {
+        hours = '0' + hours;
+      }
+      if (minutes < 10) {
+        minutes = '0' + minutes;
+      }
+      if (seconds < 10) {
+        seconds = '0' + seconds;
+      }
+      this.curTime.innerText = minutes + ':' + seconds;
+      if (this.isPlaying) {
+        this.timeLine.value = this.audio.currentTime;
+      }
+      if (sec_num >= this.timeLine.max) {
+        this.audio.pause();
+        this.playBtn.classList.remove('player__btn-play--pause');
+        this.curTime.innerHTML = '00:00';
+        this.timeLine.value = 0;
+        if (this.audioSource.length > 1) {
+          this.audio.src = this.audioSource[this.songsCounter];
+          this.playBtn.classList.toggle('player__btn-play--pause');
+          // this.audio.addEventListener('loadedmetadata', () => {
+            this.fullTime.innerHTML = `${Math.round(this.timeLine.max / 60)}:${Math.round(this.timeLine.max % 60)}`;
+            this.audio.play();
+          // });
+          this.songsCounter++;
         }
-        if (hours < 10) {
-          hours = '0' + hours;
-        }
-        if (minutes < 10) {
-          minutes = '0' + minutes;
-        }
-        if (seconds < 10) {
-          seconds = '0' + seconds;
-        }
-        this.curTime.innerText = minutes + ':' + seconds;
-        if (this.isPlaying) {
-          this.timeLine.value = song.currentTime;
-        }
-        if (sec_num >= this.timeLine.max) {
-          song.pause();
-          this.playBtn.classList.remove('player__btn-play--pause');
-          this.curTime.innerHTML = '00:00';
-          this.timeLine.value = 0;
-        }
-      });
-    }
+      }
+    });
   }
-
-  // ADD here method for playing next song from an array of songs and stop when the last song played
 }
 
 const heroPlayer = new Player(
   document.querySelector('.hero__audio-container'),
-  document.querySelectorAll('.hero__audio'),
+  document.querySelector('.hero__audio'),
+  ['audio/war for love.mp3'],
   document.querySelector('.hero__btn-play'),
   document.querySelector('.hero__cur-time-line'),
   document.querySelector('.hero__play-time'),
   document.querySelector('.hero__full-time')
+);
+
+const tracksPlayer = new Player(
+  document.querySelector('.tracks__audio-container'),
+  document.querySelector('.tracks__audio'),
+  ['audio/3Lau Feat. Bright Lights  -  How You Love Me.mp3',
+    'audio/war for love.mp3',
+    'audio/Benny Benassi & Pink Is Punk feat. Bright Lights - Ghost (Razihel Remix).mp3',
+    'audio/Hardwell &amp; Dyro feat. Bright Lights — Never Say Goodbye (Radio Edit).mp3',
+    'audio/zeds_dead_and_dirtyphonics-where_are_you_now_feat_bright_lights.mp3',
+    'audio/Zedd, Bright Lightsm - Follow You Down (Original Mix).mp3'],
+  document.querySelector('.tracks__btn-play'),
+  document.querySelector('.tracks__cur-time-line'),
+  document.querySelector('.tracks__play-time'),
+  document.querySelector('.tracks__full-time')
 );
