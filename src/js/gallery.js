bbba8fd97d8a49109996107ecd43eb54bf799619
Alex Gorodov <a.gorodov.22@gmail.com>
2022-11-18 09:11:24 +0200

    animation

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,7 @@
 let ww = window.innerWidth;
 let gw = document.querySelector('.gallery__list').scrollWidth;
+
+
 if (ww < 1150) {
   const galleryRunning = document.querySelector('.gallery__list').animate(
     [
@@ -12,6 +14,23 @@ if (ww < 1150) {
       fill: 'backwards',
       easing: 'cubic-bezier(0.4, 0.5, 0.4, 0.5)',
     });
+
+    const animPause = function() {
+      if(galleryRunning.playState === 'running') {
+        galleryRunning.pause();
+      } else {
+        galleryRunning.play();
+      }
+    }
+
+    document.querySelector('.gallery__list').addEventListener('click', () => {
+      animPause();
+    });
+
+    document.querySelector('.gallery__list').addEventListener('touch', () => {
+      animPause();
+    });
 }
+
 
 console.log(ww);
