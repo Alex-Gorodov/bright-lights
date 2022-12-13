c9d1c608c657da0488d52db79ea1a66543ed4a05
Alex Gorodov <a.gorodov.22@gmail.com>
2022-12-13 20:55:49 +0200

    slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,190 +1,46 @@
-let slider = document.querySelector('.tours'),
-  sliderList = document.querySelector('.tours__track'),
-  sliderTrack = document.querySelector('.tours__list'),
-  slides = document.querySelectorAll('.tours-card'),
-  arrows = document.querySelector('.tours__buttons-wrapper'),
-  prev = arrows.children[0],
-  next = arrows.children[1],
-  slideWidth = slides[0].offsetWidth,
-  slideIndex = 0,
-  posInit = 0,
-  posX1 = 0,
-  posX2 = 0,
-  posY1 = 0,
-  posY2 = 0,
-  posFinal = 0,
-  isSwipe = false,
-  isScroll = false,
-  allowSwipe = true,
-  transition = true,
-  nextTrf = 0,
-  prevTrf = 0,
-  lastTrf = --slides.length * slideWidth,
-  posThreshold = slides[0].offsetWidth * 0.35,
-  trfRegExp = /([-0-9.]+(?=px))/,
-  getEvent = function() {
-    return (event.type.search('touch') !== -1) ? event.touches[0] : event;
-  },
+const slider = document.querySelector('.tours__track');
+const sliderList = document.querySelector('.tours__list');
+const slideWidth = document.querySelector('.tours__card').clientWidth;
+const slides = document.querySelectorAll('.tours__card');
+const sliderLeft = document.querySelector('.tours__button--prev');
+const sliderRight = document.querySelector('.tours__button--forward');
+let index = 0;
+let position = slides[0].getBoundingClientRect().left;
+const startingTransform = (ww - slideWidth) / 2 - 20;
 
-  slide = function() {
-    slides.forEach(item => {
-      item.style.opacity = '0';
-    });
+console.log(startingTransform);
 
-    if (transition) {
-      sliderTrack.style.transition = 'transform .5s';
-    }
-    sliderTrack.style.transform = `translate3d(-${slideIndex * slideWidth + slideIndex * 30 - slideWidth * 0.5 + 30}px, 0px, 0px)`;
-    slides[slideIndex].style.opacity = '1';
+if (ww < 900) {
+    slides[index].style.opacity = '1';
+    sliderList.style.transform = `translateX(${startingTransform}px)`;
+}
 
-    console.log(slideIndex);
-
-    if (slideIndex === 0) {
-      sliderTrack.style.transform = `translate3d(${slideWidth * 0.5 - 30}px, 0px, 0px)`;
-    }
-
-    prev.classList.toggle('tours__button--disabled', slideIndex === 0);
-    next.classList.toggle('tours__button--disabled', slideIndex === --slides.length);
-  },
-  swipeStart = function() {
-    let evt = getEvent();
-
-    if (allowSwipe) {
-
-      transition = true;
-
-      nextTrf = (slideIndex + 1) * -slideWidth;
-      prevTrf = (slideIndex - 1) * -slideWidth;
-
-      posInit = posX1 = evt.clientX;
-      posY1 = evt.clientY;
-
-      sliderTrack.style.transition = '';
-
-      document.addEventListener('touchmove', swipeAction);
-      document.addEventListener('mousemove', swipeAction);
-      document.addEventListener('touchend', swipeEnd);
-      document.addEventListener('mouseup', swipeEnd);
-    }
-  },
-  swipeAction = function() {
-    let evt = getEvent(),
-    style = sliderTrack.style.transform,
-    transform = +style.match(trfRegExp)[0];
-
-
-    posX2 = posX1 - evt.clientX;
-    posX1 = evt.clientX;
-
-    posY2 = posY1 - evt.clientY;
-    posY1 = evt.clientY;
-
-    // определение действия свайп или скролл
-    if (!isSwipe && !isScroll) {
-      let posY = Math.abs(posY2);
-      if (posY > 7 || posX2 === 0) {
-        isScroll = true;
-        allowSwipe = false;
-      } else if (posY < 7) {
-        isSwipe = true;
-      }
-    }
-
-    if (isSwipe) {
-      // запрет ухода влево на первом слайде
-      if (slideIndex === 0) {
-        if (posInit < posX1) {
-          setTransform(transform, 0);
-          return;
-        } else {
-          allowSwipe = true;
-        }
-      }
-
-      // запрет ухода вправо на последнем слайде
-      if (slideIndex === --slides.length) {
-        if (posInit > posX1) {
-          setTransform(transform, lastTrf);
-          return;
-        } else {
-          allowSwipe = true;
-        }
-      }
-
-      // запрет протаскивания дальше одного слайда
-      if (posInit > posX1 && transform < nextTrf || posInit < posX1 && transform > prevTrf) {
-        reachEdge();
-        return;
-      }
-
-      // двигаем слайд
-      sliderTrack.style.transform = `translate3d(${transform - posX2}px, 0px, 0px)`;
-    }
-
-  },
-  swipeEnd = function() {
-    posFinal = posInit - posX1;
-
-    isScroll = false;
-    isSwipe = false;
-
-    document.removeEventListener('touchmove', swipeAction);
-    document.removeEventListener('mousemove', swipeAction);
-    document.removeEventListener('touchend', swipeEnd);
-    document.removeEventListener('mouseup', swipeEnd);
-
-    if (allowSwipe) {
-      if (Math.abs(posFinal) > posThreshold) {
-        if (posInit < posX1) {
-          slideIndex--;
-        } else if (posInit > posX1) {
-          slideIndex++;
-        }
-      }
-
-      if (posInit !== posX1) {
-        allowSwipe = false;
-        slide();
-      } else {
-        allowSwipe = true;
-      }
-
+sliderLeft.addEventListener('click', () => {
+    if (index !== 1) {
+        console.log('HE 1!');
+        sliderList.style.transform = `translateX(-${(slideWidth + 30) * (index - 1) - startingTransform}px)`;
     } else {
-      allowSwipe = true;
+        console.log('1!');
+        sliderList.style.transform = `translateX(${startingTransform}px)`;
     }
-
-  },
-  setTransform = function(transform, compareTransform) {
-    if (transform >= compareTransform) {
-      if (transform > compareTransform) {
-        sliderTrack.style.transform = `translate3d(${compareTransform}px, 0px, 0px)`;
-      }
+    slides[index-1].style.opacity = '1';
+    slides[index].style.opacity = '0';
+    sliderRight.classList.remove('tours__button--disabled');
+    index--;
+    if (index === 0) {
+        sliderLeft.classList.add('tours__button--disabled');
     }
-    allowSwipe = false;
-  },
-  reachEdge = function() {
-    transition = false;
-    swipeEnd();
-    allowSwipe = true;
-  };
+    console.log(index);
+});
 
-slides[0].style.opacity = '1';
-
-sliderTrack.style.transform = `translate3d(${slideWidth * 0.5 - 30}px, 0px, 0px)`;
-
-sliderTrack.addEventListener('transitionend', () => allowSwipe = true);
-slider.addEventListener('touchstart', swipeStart);
-slider.addEventListener('mousedown', swipeStart);
-
-arrows.addEventListener('click', function() {
-
-  if (event.target.closest('.tours__button--forward')) {
-    slideIndex++;
-  } else if (event.target.closest('.tours__button--prev')) {
-    slideIndex--;
-  } else {
-    return;
-  }
-
-  slide();
+sliderRight.addEventListener('click', () => {
+    sliderList.style.transform = `translateX(-${(slideWidth + 30) * (index + 1) - startingTransform}px)`;
+    slides[index+1].style.opacity = '1';
+    slides[index].style.opacity = '0';
+    sliderLeft.classList.remove('tours__button--disabled');
+    index++;
+    if (index >= slides.length - 1) {
+        sliderRight.classList.add('tours__button--disabled');
+    }
+    console.log(index);
 });
